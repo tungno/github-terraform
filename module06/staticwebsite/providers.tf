3eee16a0ead042d8af8ee0da53e2fb3e7b702434
Tung No <111445323+tungno@users.noreply.github.com>
2024-10-07 23:02:33 +0200

    init commit

---
+++ module06/staticwebsite/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.73.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tn3"
+    storage_account_name = "sabeftstn3kx9neua2l7"
+    container_name       = "tfstate-tn3"
+    key                  = "web.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ module06/staticwebsite/terraform.tfvars
@@ -0,0 +1,4 @@
+location = "westeurope"
+rg_name = "rg-web-using-github-action-tn"
+source_content = "<h1>Web page creatd with Terraform again"
+index_document = "index.html"
